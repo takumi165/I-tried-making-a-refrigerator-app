1a4e8416b49b8ac0ab8bb8e672643f778c7ccad8
takumi165 <165takumi@gmail.com>
2026-09-06 18:01:31 +0900

    モデルの作成、メインループと入力チェックの試作

--- src/model/Stock.java
+++ src/model/Stock.java
@@ -1,5 +1,57 @@
 package model;
 
-public class Stock {
+import java.time.LocalDate;
 
+public class Stock {
+	private int id;
+	private String name;
+	private LocalDate purchaseDate;
+	private int stock;
+	private LocalDate expirationDate;
+	
+	public Stock(int id, String name, LocalDate purchaseDate, int stock, LocalDate expirationDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.purchaseDate = purchaseDate;
+		this.stock = stock;
+		this.expirationDate = expirationDate;
+	}
+	
+	public Stock() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDate getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(LocalDate purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public LocalDate getExpirationDate() {
+		return expirationDate;
+	}
+	public void setExpirationDate(LocalDate expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+	
+	
 }
--- src/util/InputUtil.java
+++ src/util/InputUtil.java
@@ -1,5 +1,25 @@
 package util;
 
-public class InputUtil {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class InputUtil {
+	//入力受付
+	static Scanner inputScanner = new Scanner(System.in);
+	
+	//
+	public static int Inputcommand(){
+		System.out.println("実行する処理の番号を入力してください\n");
+		try {
+			int inputcommand = inputScanner.nextInt();
+			if (inputcommand >= 1 && inputcommand <= 5) {
+				return inputcommand;
+			}
+			return 0;
+		} catch (InputMismatchException e) {
+			inputScanner.nextLine();
+			return -1;
+		}
+		
+	}
 }
